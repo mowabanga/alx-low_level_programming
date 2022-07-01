beba5badacbc6ac54fa76bd52cd2f5ef953ed237
mowabanga <itsmundia@gmail.com>
2022-06-30 17:25:32 -0700

    Printing with puts ()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,17 @@
+/**                                                                                                                                                                                                                                                                                             
+ * Using put () function to print instead of printf string                                                                                                                                                                                                                                      
+ * you need to first make a variable
+ * Program should end with 0                                                                                                                                                                                                                          
+*/
 #include<stdio.h>
 /**
- * Using put () function to print instead of printf string
- * you need to first make a variable
- * use char variable[] = " "
- * main is void
- * return is 0
-*/
+ * main - entry point
+ *
+ * return - always 0 (success)
+ */ 
 int main(void)
 {
-	char mystrng[] = "\"Programming is like building a multilingual puzzle \n";
+	char mystrng[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mystrng);
 
