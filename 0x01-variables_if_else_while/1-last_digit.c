d66017b9237048375fb4deb7320155ee4a4503a8
mowabanga <itsmundia@gmail.com>
2022-07-01 16:16:39 -0700

    Assigning random variable

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					/* your code goes there */
+				printf("Last digit of %d is %d %s", n, n % 10,
+						((n % 10) == 0) ? "and is 0"
+						:(((n % 10) > 5) ? "and is greeater than 5"
+							:"and is less than 6 and not 0"));
+					return (0);
+}
