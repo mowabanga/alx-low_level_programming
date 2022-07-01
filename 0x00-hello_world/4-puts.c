c5fefb30efc48374dcf3953903af71b3d252ee88
mowabanga <itsmundia@gmail.com>
2022-06-30 17:11:47 -0700

    Printing with puts ()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-	char mystrng[] = "Programming is like building a multilingual puzzle \n";
+	char mystrng[] = '"Programming is like building a multilingual puzzle \n';
 
 	puts(mystrng);
 
