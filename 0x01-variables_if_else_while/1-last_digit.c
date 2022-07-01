744c8081c34825c02540c3f8d07b662abe55e38f
mowabanga <itsmundia@gmail.com>
2022-07-01 16:20:13 -0700

    Assigning random variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,35 @@
+/* * The output of the program should be:
+ *  *  - The string: `Last digit of`
+ *   *  - Followed by (n)
+	*    *  - Followed by the string: `is`
+	*     *  - Followed by:
+	*      *      - if the last digit of (n) is greater than 5: `the string and is greater than 5`
+	*       *      - if the last digit of (n) is 0: the string: `and is 0`
+	*        *      - if the last digit of (n) is less than 6 and not 0: the string: `and is less than 6 and not 0`
+	*         *  - Followed by a new line
+	*          */
+	 
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
-int main(void)
+	/**
+	 *  *main - Entry point
+	 *   *
+	 *    *Return: Always 0 (Success/correct)
+	 *     */
+
+	int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					/* your code goes there */
-				printf("Last digit of %d is %d %s", n, n % 10,
-						((n % 10) == 0) ? "and is 0"
-						:(((n % 10) > 5) ? "and is greeater than 5"
-							:"and is less than 6 and not 0"));
-					return (0);
-}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %d is %d %s\n", n, n % 10,
+			((n % 10) == 0) ? "and is 0"
+			: (((n % 10) > 5) ? "and is greater than 5"
+				: "and is less than 6 and not 0"));
+
+	return (0);
+}i
