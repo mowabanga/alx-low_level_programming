a1feda761acfca03db6b40bfd3884b6a1ad9c4f1
mowabanga <itsmundia@gmail.com>
2022-07-04 19:26:44 -0700

    hehe

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,14 @@
-/* * The output of the program should be:
- *  *  - The string: `Last digit of`
- *   *  - Followed by (n)
-	*    *  - Followed by the string: `is`
-	*     *  - Followed by:
-	*      *      - if the last digit of (n) is greater than 5: `the string and is greater than 5`
-	*       *      - if the last digit of (n) is 0: the string: `and is 0`
-	*        *      - if the last digit of (n) is less than 6 and not 0: the string: `and is less than 6 and not 0`
-	*         *  - Followed by a new line
-	*          */
-	 
-#include <stdlib.h>
+nclude <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-	/**
-	 *  *main - Entry point
-	 *   *
-	 *    *Return: Always 0 (Success/correct)
-	 *     */
+/**
+ *  *main - Entry point
+ *   *
+ *    *Return: Always 0 (Success/correct)
+ *     */
 
-	int main(void)
+int main(void)
 {
 	int n;
 
@@ -32,4 +21,4 @@
 				: "and is less than 6 and not 0"));
 
 	return (0);
-}i
+}
