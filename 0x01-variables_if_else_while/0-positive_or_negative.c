0783ec5b3af724b02bd1d0f06891ec4e56be29a4
mowabanga <itsmundia@gmail.com>
2022-07-07 11:50:12 -0700

    is no. positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,28 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <time.h>
+/**More headers can be included here
+ * Return will be zero 
+ * Program assigns a variable each time it is executed
+ */
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-				if (n > 0)
-					printf("%d is positive\n", n);
-				else if (n == 0)
-				       printf("%d is zero\n", n);
-				else (n < 0)
-					printf("%d is negative\n", n);
-					return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	for (n = 0)
+		{
+			printf("%i is zero\n", n);
+		}
+	if (n > 0)
+		{
+			printf("%i is positive\n", n);
+
+		}
+	else (n < 0)
+		{
+			printf("%i is negative\n", n);
+		}
+	return (0);
 }
