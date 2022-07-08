4c1ce10ed3e9d14e32fb958876c832ec64295247
mowabanga <itsmundia@gmail.com>
2022-07-07 18:16:03 -0700

    0-9 print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * return - always 0
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar (i + '0');
+		i++;
+	}
+
+	_putchar ('\n');
+}
