4a9591fa0778bb7c7fd0cfe453ee8830919bc65d
mowabanga <itsmundia@gmail.com>
2022-07-15 15:41:36 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ *
+ * * _strncpy - copies two strings
+ *
+ * * @dest: first parameter
+ *
+ * * @src: char pointe
+ *
+ * * @n: intyeger
+ *
+ * * Return: char
+ *
+ * */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+		int i;
+
+
+
+			for (i = 0; i < n && src[i] != '\0'; i++)
+
+						dest[i] = src[i];
+
+
+
+				for (; i < n; i++)
+
+							dest[i] = '\0';
+
+
+
+					return (dest);
+
+}
