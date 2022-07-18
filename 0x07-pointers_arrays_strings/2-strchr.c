0957075054f882dc56e2449da322c2767832c50a
mowabanga <itsmundia@gmail.com>
2022-07-18 13:45:48 -0700

    locate character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * return - to c
+ *
+ * @s - string
+ *
+ * @c - character
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
+}
