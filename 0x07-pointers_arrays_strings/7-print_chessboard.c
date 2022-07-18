eda190a9360bd2d00cdad025ba3d2c94e1be9272
mowabanga <itsmundia@gmail.com>
2022-07-18 14:20:19 -0700

    ptinting 8x8 chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code 
+ *
+ * printing a chessboard
+ *
+ * return - always 0
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	x = 0;
+	while (x < 8)
+	{
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			if (y == 7)
+				_putchar(10);
+			y++
+		}
+		x++;
+	}
+}
