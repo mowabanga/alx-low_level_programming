0d4f53e15ba4e2c3fbf091a3650e39bcac898b4b
mowabanga <itsmundia@gmail.com>
2022-07-22 16:22:46 -0700

    Static libraries

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,18 +1,28 @@
 #include "main.h"
 
-/**main - check the code
+/**
  *
- * return - always (0)
+ * * _isdigit - checks if parameter is an uppercase character.
  *
- */
+ * *
+ *
+ * * @c: input a number
+ *
+ * *
+ *
+ * * Return: 1 if c is a digit, 0 in any other case.
+ *
+ * */
+
 int _isdigit(int c)
+
 {
-	if (c >=48, &&c, c<=57)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
 	else
 	{
-	return (0);
+		return (0);
 	}
 }
